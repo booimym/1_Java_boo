3b19216e2a8bf0bdd64341c4a8eee551a95270fc
booimym <booimym@naver.com>
2022-08-26 17:54:20 +0900

    네트워크

---
+++ 12_network/src/edu/kh/network/client/model/service/ClientRun.java
@@ -0,0 +1,12 @@
+package edu.kh.network.client.model.service;
+
+public class ClientRun {
+
+	public static void main(String[] args) {
+		
+		ClientService service = new ClientService();
+		
+//		service.clientStart();
+		
+	}
+}
---
+++ 12_network/src/edu/kh/network/server/model/service/ServerRun.java
@@ -0,0 +1,12 @@
+package edu.kh.network.server.model.service;
+
+public class ServerRun {
+
+	
+	public static void main(String[] args) {
+		
+		ServerService service = new ServerService();
+		
+//		service.serverStart();
+	}
+}
